914e97930129c5a5efbcbb81a8331855d3841f9b
jgeiger <jgeiger@gmail.com>
2008-11-11 16:13:16 -0600

    thin/rack can't accept non-string headers

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   private
     def no_cache
       response.headers["Last-Modified"] = Time.now.httpdate
-      response.headers["Expires"] = 0
+      response.headers["Expires"] = "0"
       # HTTP 1.0
       response.headers["Pragma"] = "no-cache"
       # HTTP 1.1 ‘pre-check=0, post-check=0′ (IE specific)
