d4fd278208b46acba0598c63102f79f8d7655a2c
jgeiger <jgeiger@gmail.com>
2008-11-17 08:40:56 -0600

    add hoptoad notifier to log error reports

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,6 +3,8 @@
 
 class ApplicationController < ActionController::Base
 
+  include HoptoadNotifier::Catcher
+
   helper :all # include all helpers, all the time
 
   before_filter :no_cache
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,7 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '6a549b75ff133030063bf912f55f0b84'
+  config.environment_filters << "AWS_SECRET"
+  config.environment_filters << "EC2_PRIVATE_KEY"
+  config.environment_filters << "AWS_ACCESS"
+  config.environment_filters << "EC2_CERT"
+end
