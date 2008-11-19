76b7e55aa51e63c5a99eb9939b0fa02a07231f83
jgeiger <jgeiger@gmail.com>
2008-11-19 14:43:03 -0600

    ignore the sigterms

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,6 +1,7 @@
 HoptoadNotifier.configure do |config|
   config.api_key = '6a549b75ff133030063bf912f55f0b84'
   config.ignore << ArgumentError
+  config.ignore << SignalException
   config.environment_filters << "AWS_SECRET"
   config.environment_filters << "EC2_PRIVATE_KEY"
   config.environment_filters << "AWS_ACCESS"
