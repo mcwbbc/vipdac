8eed66f9b81e2a94bda5a38a2fbbabbe23a2b65e
jgeiger <jgeiger@gmail.com>
2009-01-20 12:23:39 -0600

    add spec descriptions for search databases for the functionality we expect to have

--- spec/models/search_database_spec.rb
+++ spec/models/search_database_spec.rb
@@ -20,6 +20,38 @@ describe SearchDatabase do
     end
   end
 
+  describe "reformat database" do
+    it "should reformat the UniProt header line"
+  end
+
+  describe "formatdb database" do
+    it "should run formatdb on the database"
+  end
+
+  describe "convert database" do
+    it "should convert the database for use with the ez2 processing scripts"
+  end
+
+  describe "upload converted databases to S3" do
+    it "should send the data to S3"
+  end
+
+  describe "create simpleDB entry" do
+    it "should create a record in simpleDB for the database"
+  end
+
+  describe "remove S3 files on delete" do
+    it "should remove the database files from S3 when deleted"
+  end
+
+  describe "remove simpleDB entry" do
+    it "should remove the simpleDB record when deleted"
+  end
+
+  describe "find user databases" do
+    it "should return a list of all user databases from simpleDB"
+  end
+
   protected
     def create_search_database(options = {})
       record = SearchDatabase.new({ :name => "database_name", :keyword => "keyword", :version => "version", :user_uploaded => true, :search_database_file_name => 'search_database_file', :search_database_content_type => 'text/plain', :search_database_file_size => 20 }.merge(options))
