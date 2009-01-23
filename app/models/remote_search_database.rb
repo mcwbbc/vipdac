cd22aad06767071a1dfc45680cae3178176841f0
jgeiger <jgeiger@gmail.com>
2009-01-23 14:18:36 -0600

    create methods to connect to simpleDB for the persistence of the search databases

--- app/models/remote_search_database.rb
+++ app/models/remote_search_database.rb
@@ -1,2 +1,23 @@
 class RemoteSearchDatabase < RightAws::ActiveSdb::Base
+  class << self
+    def connect
+      sdb = Aws.sdb
+      create_domain
+    end
+
+    def all
+      connect
+      RemoteSearchDatabase.find(:all)
+    end
+
+    def for_filename(filename)
+      connect
+      RemoteSearchDatabase.find_by_filename(Aws.encode(filename))
+    end
+
+    def new_for(parameters)
+      connect
+      RemoteSearchDatabase.create(parameters)
+    end
+  end
 end
--- spec/models/remote_search_database_spec.rb
+++ spec/models/remote_search_database_spec.rb
@@ -1,15 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RemoteSearchDatabase do
-  describe "save" do
-    it "shoud persist to simpleDB"
+  describe "connect" do
+    it "should create the connection to SimpleDB and create the domain" do
+      Aws.should_receive(:sdb).and_return("sdb")
+      RemoteSearchDatabase.should_receive(:create_domain).and_return(true)
+      RemoteSearchDatabase.connect
+    end
   end
 
-  describe "delete" do
-    it "should remove itself from simpleDB"
-  end
+  describe "simpleDB methods" do
+    before(:each) do
+      RemoteSearchDatabase.should_receive(:connect)
+    end
 
-  describe "find all" do
-    it "should return an array of all in simpleDB"
+    it "should return all the records" do
+      RemoteSearchDatabase.should_receive(:find).with(:all).and_return(["records"])
+      RemoteSearchDatabase.all.should == ["records"]
+    end
+
+    it "should return the record with the search_database_file_name" do
+      Aws.should_receive(:encode).with("filename").and_return("encoded")
+      RemoteSearchDatabase.should_receive(:find_by_filename).with("encoded").and_return("record")
+      RemoteSearchDatabase.for_filename("filename").should == "record"
+    end
+
+    it "should create a new record" do
+      parameters = {"name"=>"am9ibmFtZQ==", "filename"=>"aHVtYW5faXBp"}
+      RemoteSearchDatabase.should_receive(:create).with(parameters).and_return("record")
+      RemoteSearchDatabase.new_for(parameters).should == "record"
+    end
   end
 end
