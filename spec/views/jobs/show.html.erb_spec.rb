cd637c38e4c4f395b815c6028d2e144016a63d4a
jgeiger <jgeiger@gmail.com>
2009-02-06 15:16:55 -0600

    cleanup the views. remove excess information from the job index page, and make sure it appears in the show page. remove click to view text

--- spec/views/jobs/show.html.erb_spec.rb
+++ spec/views/jobs/show.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "/jobs/show.html.erb" do
   
   before(:each) do
     @job = mock_model(Job)
-
+    datafile = mock("datafile", :uploaded_file_name => "uploaded.mgf")
     working = mock("working", :recent => [], :size => 0)
     complete = mock("complete", :recent => [], :size => 0)
   
@@ -13,10 +13,15 @@ describe "/jobs/show.html.erb" do
     @job.stub!(:chunks).and_return(chunks)
     @job.stub!(:status).and_return("pending")
     @job.stub!(:searcher).and_return("tandem")
+    @job.stub!(:datafile).and_return(datafile)
+    @job.stub!(:priority).and_return(100)
     @job.stub!(:launched_at).and_return(Time.now.to_f)
     @job.stub!(:finished_at).and_return(Time.now.to_f)
     @job.stub!(:name).and_return("name")
     @job.stub!(:spectra_count).and_return(200)
+    @job.stub!(:maximum_chunk_time).and_return(200)
+    @job.stub!(:minimum_chunk_time).and_return(200)
+    @job.stub!(:average_chunk_time).and_return(200)
 
     assigns[:job] = @job
   end
