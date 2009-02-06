bef4fe190ea2654412204a3ec7072e3196b7a4e4
jgeiger <jgeiger@gmail.com>
2009-02-06 15:15:31 -0600

    make the progress bar wider

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -6,7 +6,7 @@ module JobsHelper
     total = job.chunks.first.chunk_count.to_f
     complete = job.chunks.complete.size.to_f
     percent = ((complete/total)*100).to_i
-    html << image_tag("layout/bar_left.gif", :border => 0)+image_tag("layout/bar_mid.gif", :width=> percent, :height => 10, :border => 0)+image_tag("layout/bar_right.gif", :border => 0)
+    html << image_tag("layout/bar_left.gif", :border => 0)+image_tag("layout/bar_mid.gif", :width => percent*2, :height => 10, :border => 0)+image_tag("layout/bar_right.gif", :border => 0)
     html << " #{percent}%"
     html
   end
